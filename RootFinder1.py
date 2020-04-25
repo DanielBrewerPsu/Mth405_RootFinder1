34fc8ef3ee9938c9088ca1b09700ca59fdb06986
Daniel Brewer <44036901+DanielBrewerPsu@users.noreply.github.com>
2020-04-25 16:58:10 -0700

    Improved messages explaining why program terminated
    "Got enough accuracy" vs "ran enough iterations"

--- RootFinder1.py
+++ RootFinder1.py
@@ -9,13 +9,13 @@
 
 
 def f(x):
-    #return -1*x**3 + 10*x + 15 # The function in HW8
+    return -1*x**3 + 10*x + 15 # The function in HW8
 
     #This approximates the function in worksheet 8, a discontinuous graph
-    if x <= 3.6875:
-        return 5
-    else:
-        return -5
+    # if x <= 3.6875:
+    #     return 5
+    # else:
+    #     return -5
 
 
 # Initialize search parameters
@@ -30,8 +30,8 @@ k = 3       # Number of digits to get to match
 minimumDeltaX = 1.0 / (2 * 10**(k + 1)) # 1/2 of the decimal place we care about
 
 # Print column headers
-print("n Step   a_n left    f(a_n) b_n right  f(b_n) c_n mid      f(c_n) a-b delta" +
-      "  error in f")
+print("n Step   a_n left    f(a_n) b_n right  f(b_n) c_n mid     f(c_n) a-b delta" +
+      "   error in f")
 
 kMaxIterations = 50
 for n in range(1, kMaxIterations):
@@ -50,8 +50,7 @@ for n in range(1, kMaxIterations):
           rowData)
 
     if deltaX < minimumDeltaX:
-        print("Exiting after finding " + str(k) + " matching digits.")
-        print(("The approximate root is {: 2." + str(k + 1) + "}").format(c_n))
+        print("Exiting after finding " + str(k) + " matching digits...")
         break
     else:
         # Move either b_n+1 or a_n+1 to c_n, depending on which one's sign matches f(c_n)
@@ -63,7 +62,7 @@ for n in range(1, kMaxIterations):
                     b_n = c_n
                 else:
                     print("Something went wrong because both f(a_n) < 0 and f(b_n) < 0")
-                    exit(2)
+                    break
         else:   # f_c_n < 0:
             if f_a_n < 0:
                 a_n = c_n
@@ -72,7 +71,6 @@ for n in range(1, kMaxIterations):
                     b_n = c_n
                 else:
                     print("Something went wrong because both f(a_n) >= 0 and f(b_n) >=  0")
-                    exit(2)
+                    break
 
-print("Stopping looking after " + str(n) + " iterations.")
 print(("The approximate root is {: 2." + str(k + 1) + "}").format(c_n))
